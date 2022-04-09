7f0ffab805d98250b53ee1edd5de171a32ca8673
Mason Wagoner <mgwagoner@gmail.com>
2022-04-09 09:23:59 -0400

    first commit

--- public/index.js
+++ public/index.js
@@ -4,6 +4,14 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=6b586ebffb304cb0acfda2e87423ff46`)
+
+    const result = await response.json();
+
+    const { GME, MSFT, DIS, BNTX } = result;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+
 }
 
 main()
